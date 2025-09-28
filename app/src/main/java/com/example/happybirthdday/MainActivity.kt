ba7d01c1b86d1a31e19fe55b7d7cd9d191be0726
Renan Bispo <renanjesusbispo@gmail.com>
2025-09-28 15:36:55 -0300

    adicionando imagem por baixo da mensagem de felicitação

--- app/src/main/java/com/example/happybirthdday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdday/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,6 +36,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GreentingText("Happy birthday Renan!", "From Emma")
+                    GreetingImage("Happy birthday Renan!", "From Emma")
                 }
             }
         }
@@ -41,12 +45,13 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun GreentingText(messsage: String, from: String, modifier: Modifier = Modifier) {
+fun GreentingText(message: String, from: String, modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
-        modifier = modifier.padding(8.dp)) {
+        modifier = modifier.padding(8.dp)
+    ) {
         Text(
-            text = messsage,
+            text = message,
             fontSize = 100.sp,
             lineHeight = 116.sp,
             textAlign = TextAlign.Center
@@ -65,10 +70,33 @@ fun GreentingText(messsage: String, from: String, modifier: Modifier = Modifier)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthddayTheme {
+        GreetingImage(
+            message = "Happy birthday Renan!",
+            from = "From Emma"
+        )
         GreentingText(
-            messsage = "Happy birthday Renan!",
+            message = "Happy birthday Renan!",
             from = "From Emma",
             modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+
+        GreentingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
         )
     }
 }
